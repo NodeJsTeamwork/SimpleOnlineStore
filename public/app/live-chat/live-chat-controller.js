70db01768e2ee72855918c4ae324c42614a49725
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-07 13:05:32 +0200

    live-chat anguular controller and live-chat Jade View added.

---
+++ public/app/live-chat/live-chat-controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    function LiveChatController() {
+    	var vm = this;
+
+    	
+    }
+
+    angular.module('myApp.controllers')
+        .controller('LiveChatController', [LiveChatController]);
+}()); 
