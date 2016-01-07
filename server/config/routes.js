667a5dc21823766244076452806499ccbfe50519
Novkirishki <novkirishki94@gmail.com>
2016-01-07 16:52:47 +0200

    Changed admin urls

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,9 +9,9 @@ module.exports = function(app) {
     app.get('/logout', auth.logout);
     app.get('/login', controllers.users.getLogin);
 
-    app.get('/products/add', controllers.products.getAdd);
-    app.post('/products', controllers.products.createProduct);
-    app.get('/products/my', controllers.products.getProductsByUser);
+    app.get('/products/admin/add', controllers.products.getAdd);
+    app.post('/products/admin/add', controllers.products.createProduct);
+    app.get('/products/admin/my', controllers.products.getProductsByUser);
     
     app.get('/live-chat', controllers.liveChat.getChat);
 
