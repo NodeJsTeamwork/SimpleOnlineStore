2f101a09984e9228855705210f2a4ebe1808eb83
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-08 18:47:12 +0200

    Cart page fixed.

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,88 +4,88 @@ var encryption = require('../utilities/cripto'),
 
 module.exports = {
     getRegister: function (req, res, next) {
-      if (req.user) {
-        res.redirect('/');
-      }
-
-      else {
-        res.render('users/register');
-      }
-    },
-    createUser: function(req, res, next) {
-        var newUserData = req.body;
-
-        if (newUserData.password !== newUserData.confirmPassword) {
-          req.session.error = 'Passwords do not match!';
-          res.redirect('/register');
+        if (req.user) {
+            res.redirect('/');
         }
 
         else {
-          newUserData.salt = encryption.generateSalt();
-          newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
-          usersData.createUser(newUserData, function(err, user) {
-              if (err) {
-                req.session.error = 'Username exists!';
-                res.redirect('/register');
-                return;
-              }
+            res.render('users/register');
+        }
+    },
+    createUser: function (req, res, next) {
+        var newUserData = req.body;
 
-              req.logIn(user, function(err) {
-                  if (err) {
-                      res.status(400);
-                      return res.send({reason: err.toString()});
-                  }
+        if (newUserData.password !== newUserData.confirmPassword) {
+            req.session.error = 'Passwords do not match!';
+            res.redirect('/register');
+        }
 
-                  else {
-                    res.redirect('/');
-                  }
-              });
-          });
+        else {
+            newUserData.salt = encryption.generateSalt();
+            newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
+            usersData.createUser(newUserData, function (err, user) {
+                if (err) {
+                    req.session.error = 'Username exists!';
+                    res.redirect('/register');
+                    return;
+                }
+
+                req.logIn(user, function (err) {
+                    if (err) {
+                        res.status(400);
+                        return res.send({ reason: err.toString() });
+                    }
+
+                    else {
+                        res.redirect('/');
+                    }
+                });
+            });
         };
     },
-    updateUser: function(req, res, next) {
+    updateUser: function (req, res, next) {
         if (req.user._id == req.body._id || req.user.roles.indexOf('admin') > -1) {
             var updatedUserData = req.body;
             if (updatedUserData.password && updatedUserData.password.length > 0) {
                 updatedUserData.salt = encryption.generateSalt();
                 updatedUserData.hashPass = encryption.generateHashedPassword(updatedUserData.salt, updatedUserData.password);
             }
-                      
-            if (updatedUserData.password !== updatedUserData.confirmPassword) {     
-                req.session.error = 'Passwords do not match!';      
+
+            if (updatedUserData.password !== updatedUserData.confirmPassword) {
+                req.session.error = 'Passwords do not match!';
                 res.redirect('/profile');
             } else {
-                usersData.updateUser({_id:req.body._id} , updatedUserData, function (err, user) {
+                usersData.updateUser({ _id: req.body._id }, updatedUserData, function (err, user) {
                     res.redirect('/profile');
-                })     
-            }           
+                })
+            }
         }
         else {
-            res.send({reason: 'You do not have permissions!'})
+            res.send({ reason: 'You do not have permissions!' })
         }
     },
     getLogin: function (req, res, next) {
-      if (req.user) {
-        res.redirect('/');
-      }
-      else {
-        res.render('users/login');
-      }
+        if (req.user) {
+            res.redirect('/');
+        }
+        else {
+            res.render('users/login');
+        }
     },
     getProfile: function (req, res, next) {
         if (!req.user) {
             res.redirect('/');
         } else {
-            res.render('profile/profile', { currentUser: req.user, userToUpdate: req.user});
+            res.render('profile/profile', { currentUser: req.user, userToUpdate: req.user });
         }
     },
     getAll: function (req, res, next) {
-        User.find().exec(function(err, users) {
+        User.find().exec(function (err, users) {
             if (err) {
                 console.log('Users could not be loaded: ' + err);
             };
-            
-            res.render('users/users', {currentUser: req.user, users: users});  
+
+            res.render('users/users', { currentUser: req.user, users: users });
         });
     },
     getProfileByAdmin: function (req, res, next) {
@@ -93,22 +93,31 @@ module.exports = {
         if (!userId) {
             res.redirect('/');
         }
-        
-        User.findById(userId, function(err, user) {
+
+        User.findById(userId, function (err, user) {
             if (err) {
                 console.log('User could not be loaded: ' + err);
                 req.session.error = 'User with this id does not exist';
                 res.redirect('/');
             };
-            
-            res.render('profile/profile', {currentUser: req.user, userToUpdate: user});   
+
+            res.render('profile/profile', { currentUser: req.user, userToUpdate: user });
         });
     },
     getCart: function (req, res, next) {
         if (!req.user) {
             res.redirect('/');
         } else {
-            res.render('cart/cart', { currentUser: req.user, cart: req.user.products});
+            console.log(req.user.products);
+            User.findById({ _id: req.user._id })
+                .populate('products')
+            .exec(function (err, user) {
+                if (err) {
+                    console.log('Users could not be loaded: ' + err);
+                };
+                
+                res.render('cart/cart', { currentUser: req.user, cart: user.products });
+            });
         }
-    },
-};
+    }
+    };
