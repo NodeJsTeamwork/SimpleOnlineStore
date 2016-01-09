5953fad228a818b55ee71beb182e3e00898284b2
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-09 21:25:33 +0200

    Added function that take lastest 10 products in productsData.

--- server/data/productsData.js
+++ server/data/productsData.js
@@ -3,5 +3,12 @@ var Product = require('mongoose').model('Product');
 module.exports = {
   createProduct: function (product, callback) {
     Product.create(product, callback);
+  },
+  getLatestProducts: function (callback) {
+      Product.find()
+      // the sort parameter will be dateOfCreation or rating
+             .sort('name')
+             .limit(10)
+             .exec(callback);
   }
 };
