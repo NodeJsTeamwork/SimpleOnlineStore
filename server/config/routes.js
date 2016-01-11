b4f9b08b062e524624e4bb0127a5debc7262a351
Novkirishki <novkirishki94@gmail.com>
2016-01-11 16:28:32 +0200

    Moved categories menu to the left side, reordered routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,22 +11,24 @@ module.exports = function(app) {
 
     app.get('/admin/products/add', controllers.products.getAdd);
     app.post('/admin/products/add', controllers.products.createProduct);
+    app.get('/admin/users', controllers.users.getAll);
+    app.get('/admin/profile', controllers.users.getProfileByAdmin);
 
     app.get('/cart/remove', controllers.products.removeFromCart);
-    app.get('/products', controllers.products.getProducts);
+    app.get('/cart/add', controllers.users.getAddCartConfirmation);
+    app.get('/cart', controllers.users.getCart);
     
-    app.get('/admin/users', controllers.users.getAll);
+    app.get('/checkout', controllers.users.getCheckout);
+    
+    app.get('/products', controllers.products.getProducts);
     
     app.get('/live-chat', controllers.liveChat.getChat);
     
     app.get('/profile', controllers.users.getProfile);
+    app.post('/profile', controllers.users.updateUser);
+    
     app.post('/admin/cart/add', controllers.users.addItemToCart);
     app.post('/admin/cart/remove', controllers.users.removeItemFromCart);
-    app.get('/cart/add', controllers.users.getAddCartConfirmation);
-    app.get('/cart', controllers.users.getCart);
-    app.get('/checkout', controllers.users.getCheckout);
-    app.get('/admin/profile', controllers.users.getProfileByAdmin);
-    app.post('/profile', controllers.users.updateUser);
 
     app.get('/', function (req, res) {
       res.render('index', {currentUser: req.user});
