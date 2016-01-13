a8a5ad178494e3c8f6a9f48fc96753f5debbe10b
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-13 20:36:41 +0200

    remove unnecessary commented code in liveChatController

--- server/controllers/liveChatController.js
+++ server/controllers/liveChatController.js
@@ -4,14 +4,6 @@ module.exports = function (messagesData) {
             messagesData.getAllMessages(function (err, messages) {
                 res.render('live-chat/live-chat', { currentUser: req.user, messages: messages });
             });
-            
-            // Message.find()
-            //     .exec(function (err, messages) {
-            //         return messages;
-            //     })
-            //     .then(function (messages) {
-            //         res.render('live-chat/live-chat', { currentUser: req.user, messages: messages });
-            //     });
         }
     };
     
